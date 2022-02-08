b8a2be8737a968006226043df5ab979e585797ad
AncientSoup <craftydaman7@gmail.com>
2022-02-08 20:36:24 +0800

    Create a new list function working

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,25 @@
+from datetime import datetime
+from enum import auto
+from time import timezone
+from tkinter import CASCADE
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+class Product(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.CharField(max_length=64, default=None)
+    title = models.CharField(max_length=128)
+    image = models.URLField(max_length=500, blank=True, null=True)
+    price = models.DecimalField(max_digits=32, decimal_places=2)
+    category = models.CharField(max_length=11)
+    date = models.DateTimeField(default=datetime.now())
+
+class List(models.Model):
+    item = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+class Bids(models.Model):
+    item = models.ForeignKey(Product, on_delete=models.CASCADE)
--- auctions/views.py
+++ auctions/views.py
@@ -1,10 +1,12 @@
+from turtle import title
+from unicodedata import name
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import *
 
 
 def index(request):
@@ -61,3 +63,20 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+def new(request):
+    return render(request, "auctions/new_list.html")
+
+def create_list(request):
+    if request.method == "POST":
+        u = request.POST["user"]
+        n = request.POST["itemtitle"]
+        i = request.POST["image"]
+        p = request.POST["startprice"]
+        c = request.POST["categories"]
+
+        prod = Product(user=u, title=n, image=i,price=p, category=c)
+        prod.save()
+
+        return HttpResponseRedirect(reverse("index"))
